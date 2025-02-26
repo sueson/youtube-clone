c572b2fae094fbf61c4f897aee27c78df4b47b28
sueson <sueson003@gmail.com>
2025-02-26 05:46:43 +0000

    added main section for sidebar

--- src/modules/home/ui/components/home-sidebar/index.tsx
+++ src/modules/home/ui/components/home-sidebar/index.tsx
@@ -1,7 +1,12 @@
-import { Sidebar } from "@/components/ui/sidebar"
+import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import { MainSection } from "./main-section"
 
 export const HomeSidebar = () => {
     return (
-        <Sidebar />
+        <Sidebar className="pt-16 z-40 border-none">
+            <SidebarContent className="bg-background">
+                <MainSection />
+            </SidebarContent>
+        </Sidebar>
     )
 }
