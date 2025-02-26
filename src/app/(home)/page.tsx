54d254212e6f01c03657f3f912f6f7c8b4784a36
sueson <sueson003@gmail.com>
2025-02-26 06:05:29 +0000

    modified

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,15 +1,9 @@
-import Image from "next/image";
+
 
 
 export default function Home() {
   return (
     <div>
-        <Image 
-            src={"/logo.svg"}
-            alt="logo"
-            width={50}
-            height={50}
-        />
         <p className="text-lg font-bold tracking-tight">
             Youtube
         </p>
