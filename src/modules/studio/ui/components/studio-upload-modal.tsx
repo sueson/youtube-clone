8ae65696f6d40d827c589b921ffd1b3d22797ae4
sueson <sueson003@gmail.com>
2025-02-27 20:45:28 +0000

    fix the button to show loader if isPending

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -3,16 +3,21 @@
 
 import { Button } from "@/components/ui/button"
 import { trpc } from "@/trpc/client";
-import { PlusIcon } from "lucide-react"
+import { Loader2Icon, PlusIcon } from "lucide-react"
 
 
 export const StudioUploadModal = () => {
-    const create = trpc.videos.create.useMutation();
+    const utils = trpc.useUtils();
+    const create = trpc.videos.create.useMutation({
+        onSuccess: () => {
+            utils.studio.getMany.invalidate();
+        },
+    });
 
     return (
-        <Button variant="secondary" onClick={() => create.mutate()}>
-            <PlusIcon />
+        <Button variant="secondary" onClick={() => create.mutate()} disabled={create.isPending}>
+            {create.isPending ? <Loader2Icon className="animate-spin"/> : <PlusIcon />}
             Create
         </Button>
     )
-}
+};
