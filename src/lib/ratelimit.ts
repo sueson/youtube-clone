a9dad94b1ef416443866eb4857db1c1742dead72
sueson <sueson003@gmail.com>
2025-02-28 04:13:55 +0000

    changed the rate limit request

--- src/lib/ratelimit.ts
+++ src/lib/ratelimit.ts
@@ -3,6 +3,6 @@ import { redis } from "./redis";
 
 export const rateLimit = new Ratelimit({
     redis,
-    // 10 request within 10seconds will cause timeout
-    limiter: Ratelimit.slidingWindow(10, "10s"),
+    // 100 request within 10seconds will cause timeout
+    limiter: Ratelimit.slidingWindow(100, "10s"),
 })
