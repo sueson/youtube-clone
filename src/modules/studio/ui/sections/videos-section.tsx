bc696cce286e2ca391ef865707aea76f410ffef8
sueson <sueson003@gmail.com>
2025-02-28 04:14:47 +0000

    implemented infinite scroll funtionality

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
 import { trpc } from "@/trpc/client";
 import { Suspense } from "react";
@@ -18,7 +19,7 @@ export const VideosSection = () => {
 
 
 const VideosSectionSuspense = () => {
-    const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
+    const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery({
         limit: DEFAULT_LIMIT,
     }, {
         getNextPageParam: (lastPage) => lastPage.nextCursor
@@ -27,6 +28,12 @@ const VideosSectionSuspense = () => {
     return (
         <div>
             {JSON.stringify(data)}
+            <InfiniteScroll 
+                isManual
+                hasNextPage={query.hasNextPage}
+                isFetchingNextPage={query.isFetchingNextPage}
+                fetchNextPage={query.fetchNextPage}
+            />
         </div>
     )
 };
