59d7540bb2050cca49225ba6a6ee9a1774b07a3a
sueson <sueson003@gmail.com>
2025-03-01 19:33:59 +0000

    modified

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -17,6 +17,7 @@ import Link from "next/link";
 import { VideoThumbnail } from "@/modules/videos/ui/components/video-thumbnail";
 import { snakeCaseToTitle } from "@/lib/utils";
 import { format } from "date-fns";
+import { Globe2Icon, LockIcon } from "lucide-react";
 
 
 export const VideosSection = () => {
@@ -79,7 +80,17 @@ const VideosSectionSuspense = () => {
                                             </div>
                                         </TableCell>
                                         <TableCell>
-                                            visibility
+                                            <div className="flex items-center">
+                                                {
+                                                    video.visibility === "private" ? (
+                                                        <LockIcon className="size-4 mr-2"/>
+                                                    ) 
+                                                    : (
+                                                        <Globe2Icon className="size-4 mr-2"/>
+                                                    )
+                                                }
+                                                {snakeCaseToTitle(video.visibility)}
+                                            </div>
                                         </TableCell>
                                         <TableCell>
                                             <div className="flex items-center">
