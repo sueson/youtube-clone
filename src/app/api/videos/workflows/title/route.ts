57731877f61e7030f31bd130ab730dc1f0407b3b
sueson <sueson003@gmail.com>
2025-03-02 19:24:19 +0000

    modified

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -9,6 +9,25 @@ interface InputType {
     videoId: string;
 };
 
+
+// const TITLE_SYSTEM_PROMPT = `Your task is to generate an SEO-focused title for a YouTube video based on its transcript. Please follow these guidelines:
+// - Be concise but descriptive, using relevant keywords to improve discoverability.
+// - Highlight the most compelling or unique aspect of the video content.
+// - Avoid jargon or overly complex language unless it directly supports searchability.
+// - Use action-oriented phrasing or clear value propositions where applicable.
+// - Ensure the title is 3-8 words long and no more than 100 characters.
+// - ONLY return the title as plain text. Do not add quotes or any additional formatting.`;
+
+
+// const DESCRIPTION_SYSTEM_PROMPT = `Your task is to summarize the transcript of a video. Please follow these guidelines:
+// - Be brief. Condense the content into a summary that captures the key points and main ideas without losing important details.
+// - Avoid jargon or overly complex language unless necessary for the context.
+// - Focus on the most critical information, ignoring filler, repetitive statements, or irrelevant tangents.
+// - ONLY return the summary, no other text, annotations, or comments.
+// - Aim for a summary that is 3-5 sentences long and no more than 200 characters.`;
+
+
+
 // Generate thumbnail and background jobs
 export const { POST } = serve(
     async (context) => {
@@ -31,11 +50,42 @@ export const { POST } = serve(
             return existingVideo;
         });
 
+
+        // const { body } = await context.api.openai.call(
+        //     "generate-title",
+        //     {
+        //       baseURL: "https://api.deepseek.com",
+        //       token: process.env.DEEPSEEK_API_KEY!,
+        //       operation: "chat.completions.create",
+        //       body: {
+        //         model: "deepseek-chat",
+        //         messages: [
+        //           {
+        //             role: "system",
+        //             content: TITLE_SYSTEM_PROMPT,
+        //           },
+        //           {
+        //             role: "user",
+        //             content: "Hi everyone"
+        //           }
+        //         ],
+        //       },
+        //     }
+        //   );
+
+        // if (!body || !body.choices || !body.choices[0]) {
+        //     ;console.error('Unexpected API response:', body);
+        //     throw new Error('Failed to generate title: Invalid API response structure');
+        // }
+        
+        // const title = body.choices[0].message.content;
+
+
         await context.run("update-video", async () => {
             await db
                 .update(videos)
                 .set({
-                    title: "Updated background job"
+                    title: "New-video"
                 })
                 .where(and(
                     eq(videos.id, video.id),
--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -40,6 +40,7 @@ import { useRouter } from "next/navigation";
 import Image from "next/image";
 import { THUMBNAIL_FALLBACK } from "@/modules/videos/constants";
 import { ThumbnailUploadModal } from "../components/thumbnail-upload-modal";
+import { Skeleton } from "@/components/ui/skeleton";
 
 
 interface FormSectionProps {
@@ -59,7 +60,68 @@ export const FormSection = ({ videoId } : FormSectionProps) => {
 
 
 const FormSectionSkeleton = () => {
-    return <p>Loading...</p>
+    return (
+        <div>
+            <div className="flex items-center justify-between mb-6">
+                <div className="space-y-2">
+                    <Skeleton className="h-7 w-32"/>
+                    <Skeleton className="h-4 w-40"/>
+                </div>
+                <Skeleton className="h-9 w-24"/>
+            </div>
+
+            <div className="grid grid-cols-1 lg:grid-cols-5 gap-6">
+                <div className="space-y-8 lg:col-span-3">
+                    <div className="space-y-2">
+                        <Skeleton className="h-5 w-16"/>
+                        <Skeleton className="h-10 w-full"/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Skeleton className="h-5 w-24"/>
+                        <Skeleton className="h-[220px] w-full"/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Skeleton className="h-5 w-20"/>
+                        <Skeleton className="h-[84px] w-[153px]"/>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Skeleton className="h-5 w-20"/>
+                        <Skeleton className="h-10 w-full"/>
+                    </div>
+                </div>
+
+                <div className="flex flex-col gap-y-8 lg:col-span-2">
+                    <div className="flex flex-col gap-4 bg-[#F9F9F9] rounded-xl overflow-hidden">
+                        <Skeleton className="aspect-video"/>
+                        <div className="px-4 py-4 space-y-6">
+                            <div className="space-y-2">
+                                <Skeleton className="h-4 w-20"/>
+                                <Skeleton className="h-5 w-full"/>
+                            </div>
+
+                            <div className="space-y-2">
+                                <Skeleton className="h-4 w-24"/>
+                                <Skeleton className="h-5 w-full"/>
+                            </div>
+
+                            <div className="space-y-2">
+                                <Skeleton className="h-4 w-24"/>
+                                <Skeleton className="h-5 w-full"/>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="space-y-2">
+                        <Skeleton className="h-5 w-20"/>
+                        <Skeleton className="h-10 w-full"/>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 };
 
 
