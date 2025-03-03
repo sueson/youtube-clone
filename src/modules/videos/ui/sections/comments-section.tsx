39d25625d30739e336c6250030391d55ac496fdc
sueson <sueson003@gmail.com>
2025-03-03 21:57:26 +0000

    added suggestions and comments section

---
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -0,0 +1,9 @@
+
+
+export const CommentsSection = () => {
+    return (
+        <div>
+            Comments
+        </div>
+    )
+};
---
+++ src/modules/videos/ui/sections/suggestions-section.tsx
@@ -0,0 +1,9 @@
+
+
+export const SuggestionsSection = () => {
+    return (
+        <div>
+            Section
+        </div>
+    )
+};
