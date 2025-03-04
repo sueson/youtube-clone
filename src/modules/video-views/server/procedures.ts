5e229baf3714a7f726dacf59820150adf354ecf0
sueson <sueson003@gmail.com>
2025-03-04 04:36:31 +0000

    implemented procedure to handle video views

---
+++ src/modules/video-views/server/procedures.ts
@@ -0,0 +1,35 @@
+import { db } from "@/db";
+import { videoViews } from "@/db/schema";
+import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { and, eq } from "drizzle-orm";
+import { z } from "zod";
+
+
+export const videoViewsRouter = createTRPCRouter({
+    create: protectedProcedure
+        .input(z.object({ videoId: z.string().uuid() }))
+        .mutation(async ({ ctx, input }) => {
+            const { videoId } = input;
+            const { id: userId } = ctx.user;
+
+            // only for logged in user and 1 view for 1 user
+            const [existingVideoView] = await db
+                .select()
+                .from(videoViews)
+                .where(and(
+                    eq(videoViews.videoId, videoId),
+                    eq(videoViews.userId, userId)
+                ));
+
+                if(existingVideoView) {
+                    return existingVideoView;
+                }
+
+            const [createdVideoView] = await db
+                .insert(videoViews)
+                .values({ userId, videoId })
+                .returning();
+
+            return createdVideoView;
+        }),
+});
