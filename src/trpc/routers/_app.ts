9a60ef89ae6ea05b7e5eb95a6db01c3a7fa24ac6
sueson <sueson003@gmail.com>
2025-03-04 04:38:01 +0000

    added video views to trpc router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,12 +2,14 @@ import { studioRouter } from "@/modules/studio/server/procedures";
 import { createTRPCRouter } from "../init";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 import { videosRouter } from "@/modules/videos/server/procedures";
+import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 
 
 export const appRouter = createTRPCRouter({
     categories: categoriesRouter,
     studio: studioRouter,
     videos: videosRouter,
+    videoViews: videoViewsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
