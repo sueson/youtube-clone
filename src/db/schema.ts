d52dba5e32f5cd9101922ba4aec1e16ea73a44c0
sueson <sueson003@gmail.com>
2025-03-04 20:35:27 +0000

    added video reaction schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -24,6 +24,7 @@ export const users = pgTable("users", {
 export const userRelations = relations(users, ({ many }) => ({
     videos: many(videos),
     videoViews: many(videoViews),
+    videoReactions: many(videoReactions),
 }));
 
 
@@ -90,6 +91,7 @@ export const videoRelations = relations(videos, ({ one, many }) => ({
         references: [categories.id]
     }),
     views: many(videoViews),
+    reactions: many(videoReactions),
 }));
 
 
@@ -122,3 +124,37 @@ export const videoViewsRelations = relations(videoViews, ({ one }) => ({
 export const videoViewInsertSchema = createInsertSchema(videoViews);
 export const videoViewUpdateSchema = createUpdateSchema(videoViews);
 export const videoViewSelectSchema = createSelectSchema(videoViews);
+
+
+export const reactionType = pgEnum("reaction_type", ["like", "dislike"]);
+
+
+export const videoReactions = pgTable("video_reactions", {
+    userId: uuid("user_id").references(() => users.id, { onDelete: "cascade" }).notNull(),
+    videoId: uuid("video_id").references(() => videos.id, { onDelete: "cascade" }).notNull(),
+    type: reactionType("type").notNull(),
+    createdAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+}, (t) => [
+    primaryKey({
+        name: "video_reactions_pk",
+        columns: [t.userId, t.videoId]
+    }),
+]);
+
+
+export const videoReactionRelations = relations(videoReactions, ({ one }) => ({
+    users: one(users, {
+        fields: [videoReactions.userId],
+        references: [users.id]
+    }),
+    videos: one(videos, {
+        fields: [videoReactions.videoId],
+        references: [videos.id]
+    }),
+}));
+
+
+export const videoReactionInsertSchema = createInsertSchema(videoReactions);
+export const videoReactionUpdateSchema = createUpdateSchema(videoReactions);
+export const videoReactionSelectSchema = createSelectSchema(videoReactions);
