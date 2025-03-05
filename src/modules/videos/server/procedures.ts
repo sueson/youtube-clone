40a8ec2d718b28d38215eb88d09518a4235a589f
sueson <sueson003@gmail.com>
2025-03-05 05:52:29 +0000

    added subscriptions functionality

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -1,14 +1,14 @@
 import { db } from "@/db";
-import { users, videoReactions, videos, videoUpdateSchema, videoViews } from "@/db/schema";
+import { subscriptions, users, videoReactions, videos, videoUpdateSchema, videoViews } from "@/db/schema";
 import { mux } from "@/lib/mux";
 import { workflow } from "@/lib/workflow";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { and, eq, getTableColumns, inArray } from "drizzle-orm";
+import { and, eq, getTableColumns, inArray, isNotNull } from "drizzle-orm";
 import { UTApi } from "uploadthing/server";
 import { z } from "zod";
 
-// handles video crud operation
+// handles video crud operation / api
 export const videosRouter = createTRPCRouter({
     getOne: baseProcedure
         .input(z.object({ id: z.string().uuid() }))
@@ -36,12 +36,22 @@ export const videosRouter = createTRPCRouter({
                     .where(inArray(videoReactions.userId, userId ? [userId] : []))
             );
 
+            const viewerSubscriptions = db.$with("viewer_subscriptions").as(
+                db
+                    .select()
+                    .from(subscriptions)
+                    // Filters subscriptions to include only those where the viewerId matches the current user's ID, if available.
+                    .where(inArray(subscriptions.viewerId, userId ? [userId] : []))
+            )
+
             const [existingVideo] = await db
-                .with(viewerReactions)
+                .with(viewerReactions, viewerSubscriptions)
                 .select({
                     ...getTableColumns(videos),
                     user: {
-                        ...getTableColumns(users)
+                        ...getTableColumns(users),
+                        subscriberCount: db.$count(subscriptions, eq(subscriptions.creatorId, users.id)),
+                        viewerSubscribed: isNotNull(viewerSubscriptions.viewerId).mapWith(Boolean),
                     },
                     viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
                     likeCount: db.$count(
@@ -63,6 +73,7 @@ export const videosRouter = createTRPCRouter({
                 .from(videos)
                 .innerJoin(users, eq(videos.userId, users.id))
                 .leftJoin(viewerReactions, eq(viewerReactions.videoId, videos.id))
+                .leftJoin(viewerSubscriptions, eq(viewerSubscriptions.creatorId, users.id))
                 .where(eq(videos.id, input.id))
                 // .groupBy(
                 //     videos.id,
