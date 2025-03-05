9cd8f45dfc7705e8d72138a2391f183143114b58
sueson <sueson003@gmail.com>
2025-03-05 06:58:34 +0000

    added comments

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -5,6 +5,7 @@ import { videosRouter } from "@/modules/videos/server/procedures";
 import { videoViewsRouter } from "@/modules/video-views/server/procedures";
 import { videoReactionsRouter } from "@/modules/video-reactions/server/procedures";
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
+import { commentsRouter } from "@/modules/comments/server/procedures";
 
 
 export const appRouter = createTRPCRouter({
@@ -14,6 +15,7 @@ export const appRouter = createTRPCRouter({
     videoViews: videoViewsRouter,
     videoReactions: videoReactionsRouter,
     subscriptions: subscriptionsRouter,
+    comments: commentsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
