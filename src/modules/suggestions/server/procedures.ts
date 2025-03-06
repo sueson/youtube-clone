28da52071c683504dadf63a51ca26b2992d0bce6
sueson <sueson003@gmail.com>
2025-03-06 05:41:29 +0000

    modified to add video reactions and users

--- src/modules/suggestions/server/procedures.ts
+++ src/modules/suggestions/server/procedures.ts
@@ -1,8 +1,8 @@
 import { db } from "@/db";
-import { videos } from "@/db/schema";
+import { users, videoReactions, videos, videoViews } from "@/db/schema";
 import { baseProcedure, createTRPCRouter} from "@/trpc/init";
 import { TRPCError } from "@trpc/server";
-import { eq, and, or, lt, desc } from "drizzle-orm";
+import { eq, and, or, lt, desc, getTableColumns } from "drizzle-orm";
 import { z } from "zod";
 
 
@@ -34,8 +34,21 @@ export const suggestionsRouter = createTRPCRouter({
             };
 
         const data = await db
-            .select()
+            .select({
+                ...getTableColumns(videos),
+                user: users,
+                viewCount: db.$count(videoViews, eq(videoViews.videoId, videos.id)),
+                likeCount: db.$count(videoReactions, and(
+                    eq(videoReactions.videoId, videos.id),
+                    eq(videoReactions.type, "like")
+                )),
+                dislikeCount: db.$count(videoReactions, and(
+                    eq(videoReactions.videoId, videos.id),
+                    eq(videoReactions.type, "dislike")
+                )),
+            })
             .from(videos)
+            .innerJoin(users, eq(videos.userId, users.id))
             .where(and(
                 existingVideo.categoryId
                 ? eq(videos.categoryId, existingVideo.categoryId)
