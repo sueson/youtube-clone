a012a083195f98c6c63b0f93f629c8694ed7ff11
sueson <sueson003@gmail.com>
2025-03-06 05:26:43 +0000

    added suggestions router

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -7,6 +7,7 @@ import { videoReactionsRouter } from "@/modules/video-reactions/server/procedure
 import { subscriptionsRouter } from "@/modules/subscriptions/server/procedures";
 import { commentsRouter } from "@/modules/comments/server/procedures";
 import { commentReactionsRouter } from "@/modules/comment-reactions/server/procedures";
+import { suggestionsRouter } from "@/modules/suggestions/server/procedures";
 
 
 export const appRouter = createTRPCRouter({
@@ -18,6 +19,7 @@ export const appRouter = createTRPCRouter({
     subscriptions: subscriptionsRouter,
     comments: commentsRouter,
     commentReactions: commentReactionsRouter,
+    suggestionsRouter: suggestionsRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
