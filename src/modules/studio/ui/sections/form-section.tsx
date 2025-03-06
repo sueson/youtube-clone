b0a3b070ac9e6166d703bfdf2ce3bfe96a53d566
sueson <sueson003@gmail.com>
2025-03-06 12:45:51 +0000

    added revalidation for the video

--- src/modules/studio/ui/sections/form-section.tsx
+++ src/modules/studio/ui/sections/form-section.tsx
@@ -158,6 +158,16 @@ const FormSectionSuspense = ({ videoId } : FormSectionProps) => {
         }
     });
 
+    const revalidate = trpc.videos.revalidate.useMutation({
+        onSuccess: () => {
+            utils.studio.getMany.invalidate();
+            utils.studio.getOne.invalidate({ id: videoId });
+            toast.success("Video revalidated");
+        },
+        onError: () => {
+            toast.error("Something went wrong");
+        }
+    });
 
     const restoreThumbnail = trpc.videos.restoreThumbnail.useMutation({
         onSuccess: () => {
@@ -231,6 +241,10 @@ const FormSectionSuspense = ({ videoId } : FormSectionProps) => {
                                     </Button>
                                 </DropdownMenuTrigger>
                                 <DropdownMenuContent align="end">
+                                    <DropdownMenuItem onClick={() => revalidate.mutate({ id: videoId })}>
+                                        <RotateCcwIcon className="size-4 mr-2"/>
+                                        Revalidate
+                                    </DropdownMenuItem>
                                     <DropdownMenuItem onClick={() => remove.mutate({ id: videoId })}>
                                         <TrashIcon className="size-4 mr-2"/>
                                         Delete
