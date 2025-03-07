405fb0330b33535446bf1ed71019ff2931f04c91
sueson <sueson003@gmail.com>
2025-03-07 16:02:30 +0000

    fix invalidate

--- src/modules/videos/ui/components/video-reactions.tsx
+++ src/modules/videos/ui/components/video-reactions.tsx
@@ -29,6 +29,7 @@ export const VideoReactions = ({
     const like = trpc.videoReactions.like.useMutation({
         onSuccess: () => {
             utils.videos.getOne.invalidate({ id: videoId });
+            utils.playlists.getLiked.invalidate();
         },
         onError: (error) => {
             toast.error("Something went wrong");
@@ -41,6 +42,7 @@ export const VideoReactions = ({
     const dislike = trpc.videoReactions.dislike.useMutation({
         onSuccess: () => {
             utils.videos.getOne.invalidate({ id: videoId });
+            utils.playlists.getLiked.invalidate();
         },
         onError: (error) => {
             toast.error("Something went wrong");
