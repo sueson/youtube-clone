fc9de92288d0fc237ed322a028cc7125f3df1f71
sueson <sueson003@gmail.com>
2025-03-07 17:35:03 +0000

    added api option to create a playlist

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -1,11 +1,32 @@
 import { db } from "@/db";
-import { users, videoReactions, videos, videoViews } from "@/db/schema";
+import { playlists, users, videoReactions, videos, videoViews } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { and, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 // handles video crud operation / api
 export const playlistsRouter = createTRPCRouter({
+    create: protectedProcedure
+        .input(z.object({ name: z.string().min(1) }))
+        .mutation(async ({ ctx, input }) => {
+            const { name } = input;
+            const { id: userId } = ctx.user;
+
+            const [createdPlaylist] = await db
+                .insert(playlists)
+                .values({
+                    userId,
+                    name,
+                })
+                .returning();
+
+                if(!createdPlaylist) {
+                    throw new TRPCError({ code: "BAD_REQUEST" });
+                };
+
+            return createdPlaylist;
+        }),
     getHistory: protectedProcedure
     .input(
         z.object({
