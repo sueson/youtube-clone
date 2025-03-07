c3c74ce1b1412c159cbdb4a9800b03aae4a5eba3
sueson <sueson003@gmail.com>
2025-03-07 18:08:01 +0000

    fix videos -> playlists

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -34,7 +34,7 @@ export const playlistsRouter = createTRPCRouter({
                 user: users,
             })
             .from(playlists)
-            .innerJoin(users, eq(videos.userId, users.id))
+            .innerJoin(users, eq(playlists.userId, users.id))
             .where(and(
                 eq(playlists.userId, userId),
                 cursor
