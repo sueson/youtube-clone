3549a6a918d52cddbdd3cccca58bb6cb6f34ca3f
sueson <sueson003@gmail.com>
2025-03-08 06:46:15 +0000

    modified

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,9 +1,5 @@
 import { HomeLayout } from "../../modules/home/ui/layouts/home-layout";
 
-
-// Because this is a dynamic page not a static page
-export const dynamic = "force-dynamic";
-
 interface LayoutProps {
     children: React.ReactNode;
 };
