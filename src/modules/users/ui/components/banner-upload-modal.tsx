d1123a904df9657fe4ffaf04f2eb4bb83ee99a3e
sueson <sueson003@gmail.com>
2025-03-08 05:31:29 +0000

    added functionality for upload banner

---
+++ src/modules/users/ui/components/banner-upload-modal.tsx
@@ -0,0 +1,37 @@
+import { ResponsiveModal } from "@/components/responsive-modal";
+import { UploadDropzone } from "@/lib/uploadthing";
+import { trpc } from "@/trpc/client";
+
+
+interface BannerUploadModalProps {
+    userId: string;
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+};
+
+
+export const BannerUploadModal = ({
+    userId,
+    open,
+    onOpenChange
+} : BannerUploadModalProps) => {
+    const utils = trpc.useUtils();
+
+    const onClientUploadComplete = () => {
+        utils.users.getOne.invalidate({ id: userId });
+        onOpenChange(false);
+    }
+
+    return (
+        <ResponsiveModal
+            title="Upload a banner"
+            open={open}
+            onOpenChange={onOpenChange}
+        >
+            <UploadDropzone 
+                endpoint="bannerUploader"
+                onClientUploadComplete={onClientUploadComplete}
+            />
+        </ResponsiveModal>
+    )
+};
