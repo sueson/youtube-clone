782effa4da1c50f8669b2f61cd99969224fef491
sueson <sueson003@gmail.com>
2025-03-08 01:39:15 +0000

    modified api to have delete and add video to playlist options

--- src/modules/playlists/server/procedures.ts
+++ src/modules/playlists/server/procedures.ts
@@ -7,6 +7,107 @@ import { z } from "zod";
 
 // handles video crud operation / api
 export const playlistsRouter = createTRPCRouter({
+    removeVideo: protectedProcedure
+        .input(z.object({
+            playlistId: z.string().uuid(),
+            videoId: z.string().uuid(),
+        }))
+        .mutation(async ({ ctx, input }) => {
+            const { playlistId, videoId } = input;
+            const { id: userId } = ctx.user;
+
+            const [existingPlaylist] = await db
+                .select()
+                .from(playlists)
+                .where(and(
+                    eq(playlists.id, playlistId),
+                    eq(playlists.userId, userId),
+                ));
+
+                if(!existingPlaylist) {
+                    throw new TRPCError({ code: "NOT_FOUND" });
+                };
+
+            const [existingVideo] = await db
+                .select()
+                .from(videos)
+                .where(eq(videos.id, videoId));
+
+                if(!existingVideo) {
+                    throw new TRPCError({ code: "NOT_FOUND" });
+                };
+
+            const [existingPlaylistVideo] = await db
+                .select()
+                .from(playlistVideos)
+                .where(and(
+                    eq(playlistVideos.playlistId, playlistId),
+                    eq(playlistVideos.videoId, videoId)
+                ));
+
+                if(!existingPlaylistVideo) {
+                    throw new TRPCError({ code: "NOT_FOUND" });
+                };
+
+            const [deletedPlaylistVideo] = await db
+                .delete(playlistVideos)
+                .where(and(
+                    eq(playlistVideos.playlistId, playlistId),
+                    eq(playlistVideos.videoId, videoId),
+                ))
+                .returning();
+            
+            return deletedPlaylistVideo;
+        }),
+    addVideo: protectedProcedure
+        .input(z.object({
+            playlistId: z.string().uuid(),
+            videoId: z.string().uuid(),
+        }))
+        .mutation(async ({ ctx, input }) => {
+            const { playlistId, videoId } = input;
+            const { id: userId } = ctx.user;
+
+            const [existingPlaylist] = await db
+                .select()
+                .from(playlists)
+                .where(and(
+                    eq(playlists.id, playlistId),
+                    eq(playlists.userId, userId),
+                ));
+
+                if(!existingPlaylist) {
+                    throw new TRPCError({ code: "NOT_FOUND" });
+                };
+
+            const [existingVideo] = await db
+                .select()
+                .from(videos)
+                .where(eq(videos.id, videoId));
+
+                if(!existingVideo) {
+                    throw new TRPCError({ code: "NOT_FOUND" });
+                };
+
+            const [existingPlaylistVideo] = await db
+                .select()
+                .from(playlistVideos)
+                .where(and(
+                    eq(playlistVideos.playlistId, playlistId),
+                    eq(playlistVideos.videoId, videoId)
+                ));
+
+                if(existingPlaylistVideo) {
+                    throw new TRPCError({ code: "CONFLICT" });
+                };
+
+            const [createdPlaylistVideo] = await db
+                .insert(playlistVideos)
+                .values({ playlistId, videoId })
+                .returning();
+            
+            return createdPlaylistVideo;
+        }),
     getManyForVideo: protectedProcedure
     .input(
         z.object({
